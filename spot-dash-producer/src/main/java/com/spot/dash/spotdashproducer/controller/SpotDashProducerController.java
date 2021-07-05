9818a8f532d00f30c5bb7e2bbc7cbb653c5924c3
Fernando Vinícius Gianini Silva <gianini@angularlabs.com.br>
2021-07-05 02:43:59 -0300

    Getting basic tracks infos (including audio features)

--- spot-dash-producer/src/main/java/com/spot/dash/spotdashproducer/controller/SpotDashProducerController.java
+++ spot-dash-producer/src/main/java/com/spot/dash/spotdashproducer/controller/SpotDashProducerController.java
@@ -1,8 +1,8 @@
 package com.spot.dash.spotdashproducer.controller;
 
+import com.spot.dash.spotdashproducer.model.dto.TrackInfosDto;
 import com.spot.dash.spotdashproducer.service.SpotDashProducerService;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
-import com.wrapper.spotify.model_objects.specification.PlaylistTrack;
 import lombok.AllArgsConstructor;
 import org.apache.hc.core5.http.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +21,7 @@ public class SpotDashProducerController {
     private final SpotDashProducerService spotDashProducerService;
 
     @PostMapping("/get-track-infos")
-    public List<PlaylistTrack> getTrackInfos() throws IOException, ParseException, SpotifyWebApiException {
+    public List<TrackInfosDto> getTrackInfos() throws IOException, ParseException, SpotifyWebApiException {
         return spotDashProducerService.getTrackInfos();
     }
 }
