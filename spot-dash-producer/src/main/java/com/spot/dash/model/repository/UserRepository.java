af1c167e9d7f7059658c32952b7c285c046c5113
Fernando Vinícius Gianini Silva <gianini@angularlabs.com.br>
2021-07-06 01:57:32 -0300

    removed early playground code - producer

--- spot-dash-producer/src/main/java/com/spot/dash/model/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spot.dash.model.repository;
-
-import com.spot.dash.model.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-}
